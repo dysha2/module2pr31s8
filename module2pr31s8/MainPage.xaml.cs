f4b8cb8b4572cc3f60ca5848bd31fb800b1a94e3
310-04 <310-04@310-04>
2021-12-08 11:59:27 +0500

    Добавьте файлы проекта.

---
+++ module2pr31s8/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+
+namespace module2pr31s8
+{
+    public partial class MainPage : ContentPage
+    {
+        string[] Systems;
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists("file.txt"))
+            {
+
+            }
+            else
+            {
+                GoToLoginPage();
+            }
+            File.Create("file.txt");
+            Systems = new string[] { "N2", "N3", "N8", "N10", "N16" };
+            FirstSystem.ItemsSource = Systems;
+            FirstSystem.SelectedItem = Systems[0];
+            SeconsSystem.ItemsSource = Systems;
+            SeconsSystem.SelectedItem = Systems[0];
+        }
+
+        private async void GoToRegistrationPage()
+        {
+            await Navigation.PushModalAsync(new RegistrationPage());
+        }
+
+        private async void GoToLoginPage()
+        {
+            await Navigation.PushModalAsync(new LoginPage());
+            GoToRegistrationPage();
+        }
+
+        private void Operations(object sender, EventArgs e)
+        {
+            switch (FirstSystem.SelectedItem)
+            {
+                case "N10":
+                    int num = int.Parse(Number.Text);
+                    TenToBinary(num);
+                    break;
+            }
+        }
+        private void TenToBinary(int num)
+        {
+           
+        }
+    }
+}
---
+++ module2pr31s8/RegistrationPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace module2pr31s8
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Registration(object sender, EventArgs e)
+        {
+            string username = EntryUsername.Text;
+            string password = EntryPassword.Text;
+            //StreamWriter sw = new StreamWriter("file.txt");
+            //sw.Write($"{username} {password}");
+            //sw.Close();
+            Navigation.PopModalAsync();
+        }
+    }
+}
